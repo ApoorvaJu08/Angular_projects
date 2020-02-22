11ac76e0c4c7fd484b149fa5763917aa9b126f71
ApoorvaJu08 <apoorva10339@gmail.com>
2020-02-22 23:31:39 +0530

    added create emp files

--- AngularCrud/src/app/app.module.ts
+++ AngularCrud/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
