531406c96263e993a8b3e80f5c8314fe1bf70632
ApoorvaJu08 <apoorva10339@gmail.com>
2020-02-25 17:56:07 +0530

    inc worked

--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
 
   constructor() {
-   }
+    this.title = 'Angular';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
 
+  voteUp() {
+    this.votes += 1;
+    return false;
+  }
+  voteDown() {
+    this.votes -= 1;
+    return false;
+  }
   ngOnInit(): void {
   }
 
