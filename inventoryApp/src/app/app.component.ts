ddd596cedbc632e248cb6219a7846dd8d2900c15
ApoorvaJu08 <apoorva10339@gmail.com>
2020-02-28 17:35:38 +0530

    not working properly

--- inventoryApp/src/app/app.component.ts
+++ inventoryApp/src/app/app.component.ts
@@ -12,19 +12,19 @@ export class AppComponent {
       new Product(
         'MYSHOES',
         'Black Running Shoes',
-        '/assets/images/products/black-shoes.jpg',
+        '/assets/images/products/shoe.jpeg',
         ['Men', 'Shoes', 'Running Shoes'],
         109.99),
         new Product(
         'NEATOJACKET',
         'Blue Jacket',
-        '/assets/images/products/blue-jacket.jpg',
+        '/assets/images/products/jacket.jpeg',
         ['Women', 'Apparel', 'Jackets & Vests'],
         238.99),
         new Product(
         'NICEHAT',
         'A Nice Black Hat',
-        '/assets/images/products/black-hat.jpg',
+        '/assets/images/products/hat.jpeg',
         ['Men', 'Accessories', 'Hats'],
         29.99)
     ];
--- inventoryApp/src/app/product-image/product-image.component.ts
+++ inventoryApp/src/app/product-image/product-image.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../product.model';
 @Component({
   selector: 'app-product-image',
   template: `
-  <img class="product-image" [src]="product.imageUrl">
+  <!-- <img class="product-image" src="{{ product.imageUrl }}"> -->
   `,
   styleUrls: ['./product-image.component.css']
 })
 export class ProductImageComponent implements OnInit {
-
+  @Input() product: Product;
   constructor() { }
 
   ngOnInit(): void {
