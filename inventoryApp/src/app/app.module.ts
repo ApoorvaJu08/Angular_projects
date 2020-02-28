fdf700f8c5a1f0b73388caffc4285f6ac6aac944
ApoorvaJu08 <apoorva10339@gmail.com>
2020-02-28 17:01:21 +0530

    product row

--- inventoryApp/src/app/app.module.ts
+++ inventoryApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    ProductRowComponent
   ],
   imports: [
     BrowserModule,
---
+++ inventoryApp/src/app/product-row/product-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../product.model';
+@Component({
+  selector: 'app-product-row',
+  templateUrl: './product-row.component.html',
+  styleUrls: ['./product-row.component.css']
+})
+export class ProductRowComponent implements OnInit {
+  @Input() product: Product;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
