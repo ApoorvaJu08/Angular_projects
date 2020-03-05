830258b035ca48b8aef139c9f05097ee78f21d6b
ApoorvaJu08 <apoorva10339@gmail.com>
2020-03-05 10:14:53 +0530

    doing

---
+++ sandbox/src/app/components/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: User;
+  constructor() {
+   }
+
+  ngOnInit() {
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '50 main st',
+        city: 'Boston',
+        state: 'MA'
+      }
+    };
+  }
+
+}
