c701908795607937c156106845f5bd2869af5c15
ApoorvaJu08 <apoorva10339@gmail.com>
2020-03-06 12:03:59 +0530

    doing users

--- sandbox/src/app/components/users/users.component.ts
+++ sandbox/src/app/components/users/users.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../../models/user';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // setTimeout(() => {
+      this.users = [
+        { firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '50 main st',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        { firstName: 'Kevin',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '50 main st',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        { firstName: 'Mark',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '50 main st',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+      ];
+      this.loaded = true;
+    // }, 2000);
+
+      this.addUser({
+      firstName: 'David',
+        lastName: 'Doe'
+        // age: 30,
+        // address: {
+        //   street: '50 main st',
+        //   city: 'Boston',
+        //   state: 'MA'
+        // }
+    });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 
 }
