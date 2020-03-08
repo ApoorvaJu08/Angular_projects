0d3629a9c871ff833ad3858ec9a0e5470a7a4043
ApoorvaJu08 <apoorva10339@gmail.com>
2020-03-08 11:01:34 +0530

    class binding

--- sandbox/src/app/components/users/users.component.ts
+++ sandbox/src/app/components/users/users.component.ts
@@ -10,6 +10,7 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
+  currentClasses = {};
   constructor() { }
 
   ngOnInit() {
@@ -23,7 +24,8 @@ export class UsersComponent implements OnInit {
             city: 'Boston',
             state: 'MA'
           },
-          image: 'http://lorempixel.com/600/600/people/3'
+          image: 'http://lorempixel.com/600/600/people/3',
+          isActive: true
         },
         { firstName: 'Kevin',
           lastName: 'Doe',
@@ -33,7 +35,8 @@ export class UsersComponent implements OnInit {
             city: 'Boston',
             state: 'MA'
           },
-          image: 'http://lorempixel.com/600/600/people/2'
+          image: 'http://lorempixel.com/600/600/people/2',
+          isActive: false
         },
         { firstName: 'Mark',
           lastName: 'Doe',
@@ -43,26 +46,35 @@ export class UsersComponent implements OnInit {
             city: 'Boston',
             state: 'MA'
           },
-          image: 'http://lorempixel.com/600/600/people/1'
+          image: 'http://lorempixel.com/600/600/people/1',
+          isActive: true
         },
       ];
       this.loaded = true;
     // }, 2000);
 
-      this.addUser({
-      firstName: 'David',
-        lastName: 'Doe'
-        // age: 30,
-        // address: {
-        //   street: '50 main st',
-        //   city: 'Boston',
-        //   state: 'MA'
-        // }
-    });
+      // this.addUser({
+      //   firstName: 'David',
+      //   lastName: 'Doe'
+      //   // age: 30,
+      //   // address: {
+      //   //   street: '50 main st',
+      //   //   city: 'Boston',
+      //   //   state: 'MA'
+      //   // }
+      // });
+      this.setCurrentClasses();
   }
 
   addUser(user: User) {
     this.users.push(user);
+  }
+
+  setCurrentClasses() {
+    this.currentClasses = {
+      'btn-success': this.enableAdd,
+      'big-text': this.showExtended
+    }
   }
 
 }
